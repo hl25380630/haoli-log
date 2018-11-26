65359d890c0d5907df9000cdd62e391e905e49ff
lihao <lihao_100@boe.com.cn>
2018-11-26 15:19:26 +0800

    构建项目

--- haoli-log-api/src/main/java/com/haoli/LogApi.java
+++ haoli-log-api/src/main/java/com/haoli/LogApi.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 @EnableTransactionManagement
