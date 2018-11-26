ef661572497104bb3fda1afdd9f4565b40f988c7
lihao <lihao_100@boe.com.cn>
2018-11-26 16:16:10 +0800

    构建项目

--- haoli-log-api/src/main/java/com/haoli/log/api/DemoApi.java
+++ haoli-log-api/src/main/java/com/haoli/log/api/DemoApi.java
@@ -1,8 +1,14 @@
 package com.haoli.log.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoApi {
+	
+	@GetMapping("/log/hello")
+	public String hello() {
+		return "hello world";
+	}
 
 }
